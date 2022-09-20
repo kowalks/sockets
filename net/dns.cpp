8648e69b0463b58d9c29c98a401699a1bc3f31b0
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2022-09-20 19:05:49 -0300

    dns service

---
+++ net/dns.cpp
@@ -0,0 +1,41 @@
+// #include <sys/socket.h>
+// #include <sys/types.h>
+// #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <iostream>
+#include <vector>
+
+#include "dns.h"
+
+std::vector <std::string> dns_resolution(char *node, char *service) {
+    struct addrinfo hints;
+    struct addrinfo* res;
+    std::vector <std::string> ips;
+
+    // hints - modo de configurar o socket para o tipo  de transporte
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
+
+    // funcao de obtencao do endereco via DNS - getaddrinfo 
+    // funcao preenche o buffer "res" e obtem o codigo de resposta "status" 
+    int status = 0;
+    if ((status = getaddrinfo(node, service, &hints, &res)) != 0) {
+        std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+        return std::vector <std::string>();
+    }
+
+    for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
+        // a estrutura de dados eh generica e portanto precisa de type cast
+        struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+
+        // e depois eh preciso realizar a conversao do endereco IP para string
+        char ipstr[INET_ADDRSTRLEN] = {'\0'};
+        inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+        ips.push_back(std::string(ipstr));
+    }
+
+    freeaddrinfo(res);
+    return ips;
+}
---
+++ net/dns.h
@@ -0,0 +1,8 @@
+#ifndef NET_DNS_H
+#define NET_DNS_H
+
+#include <vector>
+
+std::vector <std::string> dns_resolution(char *node, char *service);
+
+#endif
